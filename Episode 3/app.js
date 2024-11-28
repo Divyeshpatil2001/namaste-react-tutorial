8da817a9393b5cbd042e446f3f07b129545d4820
Divyeshpatil2001 <patildivyesh861@gmail.com>
2024-11-28 22:03:42 +0530

    understand functional component

--- Episode 3/app.js
+++ Episode 3/app.js
@@ -13,9 +13,18 @@ const jsxheading = <h1>welcome to jsx</h1>
 console.log(heading)
 console.log(jsxheading)
 
+// functional components => js function that return some jsx or react element
+const Title = () => <h1>Namaste React</h1>
+const HeadingComponent = () => (
+<div id="container">
+ <Title />
+</div>
+)
+
 // babel is doing transplation => Converting the code in such a format that the browsers can understand.
 // parecl gives this responsibilty to babel then now browser unserstand jsx
 // jsx is html like strutcure 
 // jsx (transpiled by Babel) => React.createElement => ReactElementjs object => html element(render)
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
+root.render(heading)
+// root.render(HeadingComponent);
