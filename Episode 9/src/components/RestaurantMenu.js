cb712561e39989d1f6ca0e00397161ae23a11fad
Divyeshpatil2001 <divyesh.patil@fxis.ai>
2025-05-31 10:06:18 +0530

    created fetching menu utility custom hook separtely

--- Episode 9/src/components/RestaurantMenu.js
+++ Episode 9/src/components/RestaurantMenu.js
@@ -1,28 +1,12 @@
 import { useState,useEffect } from "react";
 import Shimmer from "./Shimmer";
-import { MENU_API } from "../utils/constant";
 import { useParams } from "react-router-dom";
+import useRestaurantMenu from "../utils/useRestaurantMenu";
 
 const RestaurantMenu = () => {
-    const [resInfo,setResInfo] = useState(null);
     const { resId } = useParams();
-    useEffect(() => {
-        fetchMenu();
-    },[])
-    const fetchMenu = async () => {
-        try{
-            const data = await fetch(MENU_API + resId);
-            const json = await data.json();
-            // console.log(json.data.cards[4].groupedCard.cardGroupMap.REGULAR.cards[1].card.card.itemCards);
-            // const menuData = json.data.cards[2].card.card.info;
-            await setResInfo(json.data);
-            // const menuTitle = json.data.cards[4].groupedCard.cardGroupMap.REGULAR.cards[1].card.card.title
-            // console.log(resInfo.cards[2].card.card.info)
-        }
-        catch (error) {
-            console.error("Error fetching menu:", error);
-        }
-    }
+    
+    const resInfo = useRestaurantMenu(resId)
     
     if (resInfo == null) return <Shimmer />
     
