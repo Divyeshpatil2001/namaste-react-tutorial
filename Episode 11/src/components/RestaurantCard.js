3d67f80b76d23d5e78355b8b2e1055b280d1fa26
Divyeshpatil2001 <divyesh.patil@fxis.ai>
2025-07-17 21:55:41 +0530

    impelemnt higher order function for promoted resstaurant enhancements

---
+++ Episode 11/src/components/RestaurantCard.js
@@ -0,0 +1,42 @@
+const RestaurantCard = ({ resData }) => {
+  const {
+    name,
+    cuisines,
+    avgRating,
+    sla,
+    costForTwo,
+    cloudinaryImageId,
+  } = resData.info;
+
+  return (
+    <div className="m-4 p-4 w-[250px] bg-gray-200 rounded-lg">
+      <img
+        className="rounded-lg"
+        src={`https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/${cloudinaryImageId}`}
+        alt={`${name}`}
+      />
+      <h3 className="font-boldpy-4 text-lg">{name}</h3>
+      <h4 className="font-boldpy-4 text-lg">{cuisines.join(", ")}</h4>
+      <h4 className="font-boldpy-4 text-lg">{avgRating} stars</h4>
+      <h4 className="font-boldpy-4 text-lg">
+        Delivery Time: {sla.deliveryTime} mins
+      </h4>
+      <h4 className="font-boldpy-4 text-lg">Cost for Two: {costForTwo}</h4>
+    </div>
+  );
+};
+
+export const WithPromotedLabel = (RestaurantCard) => {
+  return (props) => {
+    return (
+      <div>
+        <div className="relative m-4 p-4 w-[200px] bg-gray-200 rounded-lg">
+          Promoted
+        </div>
+        <RestaurantCard {...props}/>
+      </div>
+    )
+  }
+}
+
+export default RestaurantCard;
